b5a340a35903d72d80b81f5bfd1287fc876d5990
abhipray-cpu <dumkaabhipray@gmail.com>
2024-07-21 19:09:38 +0530

    exercise-service

---
+++ src/test/java/one/abhipray/Exercise_Service/ExerciseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package one.abhipray.Exercise_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
